ce7ba9556fc1d6117bb6b3558bd2cca4e2f916b8
Katiane Griza <katygriza@hotmail.com>
2022-08-09 08:18:31 -0300

    Banco de dados ok

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into animaisMarinhos(nome, especie, habitat)values('Polvo de anéis azuis', 'polvo', 'Águas australianas');
-insert into animaisMarinhos(nome, especie, habitat)values('Tubarão tigre', 'Tubarão', 'Nordeste do Brasil');
-insert into animaisMarinhos(nome, especie, habitat)values('Serpente Marinha', 'Cobras', 'Oceano Índico e Pacífico');
+insert into animais_marinhos(nome, especie, habitat)values('Polvo de anéis azuis', 'polvo', 'Águas australianas');
+insert into animais_marinhos(nome, especie, habitat)values('Tubarão tigre', 'Tubarão', 'Nordeste do Brasil');
+insert into animais_marinhos(nome, especie, habitat)values('Serpente Marinha', 'Cobras', 'Oceano Índico e Pacífico');
